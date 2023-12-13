8f13479ef060a6cd98d7fd295c0a7140b6b7006f
lnoueryo <jounetsism@gmail.com>
2023-12-13 14:36:23 +0900

    Change Directory Structure for Test

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    include: ['tests/vitest/**/*.{test,spec}.{js,ts,jsx,tsx}']
   },
   resolve: {
     alias: {
