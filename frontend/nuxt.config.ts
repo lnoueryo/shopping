1ebf4f760962619372024205d9998783cc8eb76a
lnoueryo <jounetsism@gmail.com>
2024-01-08 22:58:37 +0900

    change config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 const mode = process.env.MODE || 'development';
+console.log(mode)
 export default defineNuxtConfig({
   modules: ['@pinia/nuxt'],
   runtimeConfig: {
