07037fbdb3e5af9bbc59ed3acc9be6b6c6b3d91d
lnoueryo <jounetsism@gmail.com>
2024-01-31 14:30:18 +0900

    Add svg to each page #84

---
+++ frontend/pages/[id].vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const route = useRoute();
+const imageSrc = `/images/roadmaps/${route.params.id}.svg`; // 画像のパス
+
+</script>
+
+
+<template>
+  <img :src="imageSrc" :alt="`roadmap of ${route.params.id}`" />
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
