3571af285d9dcb0fef786fab71d4f379acc40e95
lnoueryo <jounetsism@gmail.com>
2024-02-12 20:10:57 +0900

    Add screenshot

--- frontend/tests/e2e/pages/sp/books.spec.ts
+++ frontend/tests/e2e/pages/sp/books.spec.ts
@@ -9,8 +9,8 @@ let fileName: string;
 test.describe('Books', () => {
   const FilterAccordionLabelSelector = '#float-filter label.label';
   const FilterAccordionContentSelector = FilterAccordionLabelSelector + ' ~ div.content';
-  const waitInitialDisplay = async(page) => {
-    await page.goto('/books?keyword=ノーコードでつくるWebサイト　ツール選定・デザイン・制作・運用が全部わかる！');
+  const waitInitialDisplay = async(page, path = '/books?keyword=ノーコードでつくるWebサイト　ツール選定・デザイン・制作・運用が全部わかる！') => {
+    await page.goto(path);
     await page.waitForLoadState('networkidle');
     await page.waitForSelector('.skeleton', { state: 'hidden' });
     await page.waitForSelector('.spinner-border', { state: 'hidden' });
@@ -33,10 +33,7 @@ test.describe('Books', () => {
 
   test('01_open_accordion', async ({ page }) => {
     await waitInitialDisplay(page)
-    const vh = await getStyleFromRoot(page, '--vh')
-    await waitAnimation(page, FilterAccordionContentSelector, 'maxHeight', ((Number(vh.replace('px', '')) * 100) - 120 - 40) + 'px');
-    await page.waitForSelector('.spinner-border', { state: 'hidden' });
-    await page.waitForSelector('#server-error', {state: 'hidden'});
+    await waitAccordionOpen(page)
     await page.screenshot({ path: `${BASE_IMAGE_PATH}/${browserName}/${fileName}` });
   });
 
@@ -58,7 +55,7 @@ test.describe('Books', () => {
   test('03_activate_skill_levels', async ({ page }) => {
     await waitInitialDisplay(page)
     await waitAccordionOpen(page)
-    const skillLevelFirstSelector = `${FilterAccordionContentSelector} fieldset:not(.rate-form) label.next-space`;
+    const skillLevelFirstSelector = `${FilterAccordionContentSelector} fieldset:not(.rate-form) .next-space label`;
     const skillLevelsElements = skillLevelsData.map(level => {
       const title = level.title.charAt(0).toUpperCase() + level.title.slice(1);
       return page.locator(skillLevelFirstSelector).filter({ hasText: title })
@@ -79,7 +76,7 @@ test.describe('Books', () => {
     await page.screenshot({ path: `${BASE_IMAGE_PATH}/${browserName}/${fileName}` });
   });
 
-  test.only('04_activate_genre_selector', async ({ page }) => {
+  test('04_activate_genre_selector', async ({ page }) => {
     await waitInitialDisplay(page);
     await waitAccordionOpen(page);
     const selector = `${FilterAccordionContentSelector} fieldset button[aria-label="Design genre"]`;
@@ -91,6 +88,71 @@ test.describe('Books', () => {
       const element = document.querySelector(selector);
       return window.getComputedStyle(element).backgroundColor === expectedColor;
     }, [selector, expectedColor]);
+    await page.screenshot({ path: `${BASE_IMAGE_PATH}/${browserName}/${fileName}` });
+  });
+
+  test('05_float_search_bar', async ({ page }) => {
+    const path = '/books?keyword=clean+co&page=1';
+    await waitInitialDisplay(page, path);
+    await page.waitForSelector(FilterAccordionLabelSelector);
+    const heightContent = await getStyleFromRoot(page, '--height-content');
+    const scrollY = heightContent.replace('px', '') * 2;
+    await page.evaluate((scrollY) => window.scrollTo(0, scrollY), scrollY);
+    await page.waitForFunction((scrollY) => {
+      console.log(window.scrollY)
+      return window.scrollY === scrollY;
+    }, scrollY)
+    await page.screenshot({ path: `${BASE_IMAGE_PATH}/${browserName}/${fileName}` });
+  });
+
+  test('06_hide_float_filter', async ({ page }) => {
+    const path = '/books?keyword=clean+co&page=1';
+    await waitInitialDisplay(page, path);
+    const heightContent = await getStyleFromRoot(page, '--height-content');
+    const scrollY = heightContent.replace('px', '') * 3 + 1;
+    await page.evaluate((scrollY) => window.scrollTo(0, scrollY), scrollY);
+    await page.waitForFunction((scrollY) => {
+      console.log(window.scrollY)
+      return window.scrollY === scrollY;
+    }, scrollY)
+    const floatAccordionSelector = '.float-menu';
+    const expect = 0;
+    await page.waitForFunction(({selector, expect}) => {
+      const el = document.querySelector(selector);
+      return el.getBoundingClientRect().top === expect;
+    }, {selector: floatAccordionSelector, expect})
+    await page.screenshot({ path: `${BASE_IMAGE_PATH}/${browserName}/${fileName}` });
+  });
+
+  test('07_show_float_filter', async ({ page }) => {
+    const path = '/books?keyword=clean+co&page=1';
+    await waitInitialDisplay(page, path);
+    await page.evaluate(() => {
+      let el = document.documentElement;
+      let bottom = el.scrollHeight - el.clientHeight;
+      window.scroll(0, bottom);
+    });
+    await page.waitForFunction(() => {
+      let el = document.documentElement;
+      let bottom = el.scrollHeight - el.clientHeight;
+      return window.scrollY === bottom;
+    })
+    await page.evaluate(() => {
+      let el = document.documentElement;
+      let bottom = el.scrollHeight - el.clientHeight - 10;
+      window.scroll(0, bottom);
+    });
+    await page.waitForFunction(() => {
+      let el = document.documentElement;
+      let bottom = el.scrollHeight - el.clientHeight - 10;
+      return window.scrollY === bottom;
+    })
+    const floatAccordionSelector = '.float-menu';
+    const expect = await getStyleFromRoot(page, '--height-content');
+    await page.waitForFunction(({selector, expect}) => {
+      const el = document.querySelector(selector);
+      return el.getBoundingClientRect().top === Number(expect.replace('px', ''));
+    }, {selector: floatAccordionSelector, expect})
     await page.screenshot({ path: `${BASE_IMAGE_PATH}/${browserName}/${fileName}` });
   });
 
